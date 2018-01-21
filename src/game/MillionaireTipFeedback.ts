73d957a00e1a7b22bf761cca47a95b446fc137c3
Georg Schubbauer <georg.schubbauer@edu.szu.at>
2018-01-21 19:24:22 +0100

    QuestionQ Testbereit gemacht

--- src/game/MillionaireTipFeedback.ts
+++
@@ -1,9 +0,0 @@
-﻿export class MillionaireTipFeedback {
-    constructor(
-        public Correct: boolean,
-        public Score: number,
-        public Message: string,
-        public Time: number,
-        public JokersUsed: string[]
-    ) { }
-}
