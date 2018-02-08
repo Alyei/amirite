97e1c02276b1998756fe502d429c2c4e5699b1cf
Georg Schubbauer <georg.schubbauer@edu.szu.at>
2018-02-08 17:19:50 +0100

    enabled Determination to the GameFactory

--- src/game/GameFactory.ts
+++ src/game/GameFactory.ts
@@ -11,6 +11,7 @@ import { RunningGames } from "../game/RunningGames";
 import { logger } from "../server/logging";
 import { generateGameId } from "../server/helper";
 import { GameCreationError } from "../server/Errors";
+import { DeterminationGame } from "./DeterminationGame";
 
 export class GameFactory {
   public Sessions: RunningGames;
@@ -73,14 +74,44 @@ export class GameFactory {
         }
         break;
       }
-      /*case Gamemode.Determination: {
-        return new DeterminationGame(
+      case Gamemode.Determination: {
+        const newGame: DeterminationGame = new DeterminationGame(
           generalArguments,
-          send,
-          gameEnded,
+          namespaceSocket,
           gameArguments
         );
-      }*/
+        try {
+          newGame
+            .AddPlayer(generalArguments.owner, generalArguments.ownerSocket, [
+              2
+            ])
+            .then((res: any) => {
+              logger.log(
+                "info",
+                "Added owner %s as player to game %s.",
+                generalArguments.owner,
+                generalArguments.gameId
+              );
+            })
+            .catch((err: any) => {
+              logger.log(
+                "info",
+                "Adding of player %s to game %s was unsuccessful.",
+                generalArguments.owner,
+                generalArguments.gameId
+              );
+            });
+          this.Sessions.addRunningGame(newGame);
+          logger.log(
+            "info",
+            "New QuestionQ game: %s hosted.",
+            generalArguments.gameId
+          );
+        } catch (e) {
+          logger.log("error", e.message);
+        }
+        break;
+      }
       default:
         throw new GameCreationError("Invalid gamemode passed.");
     }
